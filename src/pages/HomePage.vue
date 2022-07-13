3fa5f393b3266b1a87d4f1abd87dcefe890c0305
IsaiahWright902 <icwright902@gmail.com>
2022-07-13 13:27:26 -0400

    swtiched youtube link

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -359,7 +359,7 @@
         <iframe
           width="100%"
           height="400"
-          src="https://www.youtube.com/embed/-Pts2HtDFhM"
+          src="https://www.youtube.com/embed/o5wriLsOve0"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
